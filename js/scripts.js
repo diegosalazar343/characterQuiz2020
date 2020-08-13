67813ef299617094baffc60f5bef62d517c696a9
Jaki Widby <jaki.widby@gmail.com>
2020-08-13 15:53:44 -0700

    create determineResult function to add values and determine character

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
+
+function DetermineResult {
 const yasss = $("input:radio[name=yasss]:checked").val();
 const BBB = $("input:radio[name=BBB]:checked").val();
 const wigs = $("input:radio[name=wigs]:checked").val();
-
 const color = $("#color").val();
 const animal = $("#animal").val();
 const leader = $("#leader").val();
+const valTotal = yasss + BBB + wigs + color + animal + leader;
+let character;
+if (valTotal <= 8) {
+  character = "Ron";
+}
+
+else if (valTotal<= 16) {
+  character = "Mario"
+}
+
+else {
+  character ="Ilana"
+}
+
+}
