e12382a49ac078aea6b59f80447e2c1894491d0f
Jaki Widby <jaki.widby@gmail.com>
2020-08-13 15:38:01 -0700

    create initial html page with form, select boxes, radio buttons, and submit button

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const yasss = $("input:radio[name=yasss]:checked").val();
+const BBB = $("input:radio[name=BBB]:checked").val();
+const wigs = $("input:radio[name=wigs]:checked").val();
+
+const color = $("#color").val();
+const animal = $("#animal").val();
+const leader = $("#leader").val();
